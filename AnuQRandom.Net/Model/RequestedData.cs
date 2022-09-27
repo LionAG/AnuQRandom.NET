1db1f7629baf2c07e117a1d717f910bd121da2af
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-09-27 17:14:50 +0200

    Add project files.

---
+++ AnuQRandom.Net/Model/RequestedData.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace AnuQRandom.Model
+{
+    public class RequestedData
+    {
+        [JsonConstructor]
+        public RequestedData([JsonProperty("type")] string type,
+                             [JsonProperty("length")] int length,
+                             [JsonProperty("size")] int size,
+                             [JsonProperty("data")] List<string> data,
+                             [JsonProperty("success")] bool success)
+        {
+            Type = type;
+            Length = length;
+            Size = size;
+            Data = data;
+            Success = success;
+        }
+
+        [JsonProperty("type")]
+        public string Type { get; }
+
+        [JsonProperty("length")]
+        public int Length { get; }
+
+        [JsonProperty("size")]
+        public int Size { get; }
+
+        [JsonProperty("data")]
+        public IReadOnlyList<string> Data { get; }
+
+        [JsonProperty("success")]
+        public bool Success { get; }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,12 @@
+﻿var client = new AnuQRandom.NewClient("[API KEY]");
+var data = await client.RequestAsync();
+
+if (data != null && data.Success)
+{
+	foreach (var number in data.Data)
+	{
+		Console.WriteLine(number);
+	}
+}
+
+Console.WriteLine("Finished!");
