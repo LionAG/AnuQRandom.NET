e145e8d88d48cd97821919a035d35d86d51dc82e
Nesae <92125893+Nesae-avi@users.noreply.github.com>
2022-09-28 00:18:12 +0200

    Update example project

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,12 +1,59 @@
-﻿var client = new AnuQRandom.NewClient("[API KEY]");
-var data = await client.RequestAsync();
-
-if (data != null && data.Success)
+﻿namespace Example
 {
-	foreach (var number in data.Data)
+	public static class Program
 	{
-		Console.WriteLine(number);
+		public static async Task GetNumbers_NewApi()
+		{
+            var client = new AnuQRandom.NewApiClient("[API KEY]");
+            var data = await client.RequestAsync();
+
+            if (data != null && data.Success)
+            {
+                foreach (var number in data.Data)
+                    Console.WriteLine(number);
+            }
+        }
+
+        public static async Task GetNumbers_OldApi()
+        {
+            var client = new AnuQRandom.OldApiClient()
+            {
+                arrayLength = 0x32 // Get 50 numbers
+            };
+
+            var data = await client.RequestAsync();
+
+            if (data != null && data.Success)
+            {
+                foreach (var number in data.Data)
+                    Console.WriteLine(number);
+            }
+        }
+
+        public static async Task DirtyClientExample()
+        {
+            var client = new AnuQRandom.DirtyClient();
+
+            // Save a random 256 x 256 scatter plot image in the current application working directory.
+
+            await client.GetScatterPlotImageAsync("scatter_plot.png");
+
+            var hexBlock = await client.GetHexadecimalBlockAsync();
+            var binBlock = await client.GetBinaryBlockAsync();
+            var alpBlock = await client.GetAlphanumericBlockAsync();
+
+            Console.WriteLine("Hexadecimal block:\n\n");
+            Console.WriteLine(hexBlock);
+
+            Console.WriteLine("\n\nBinary block:\n\n");
+            Console.WriteLine(binBlock);
+
+            Console.WriteLine("\n\nAlphanumeric block:\n\n");
+            Console.WriteLine(alpBlock);
+        }
+
+        public static void Main() => GetNumbers_OldApi().GetAwaiter().GetResult();
 	}
 }
 
-Console.WriteLine("Finished!");
+
